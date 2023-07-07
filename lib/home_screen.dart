8bf136a505ff178eb99bd827dfce902691802970
mmohammadmfallahm02 <mmohammadmfallahm02@gmail.com>
2023-07-08 02:01:38 +0330

    add circle widgets

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -10,15 +10,44 @@ class HomeScreen extends StatelessWidget {
       backgroundColor: const Color(0xff1C1760),
       body: Stack(
         children: [
+          Positioned(
+              top: 130,
+              left: 220,
+              child: Container(
+                width: 300,
+                height: 300,
+                decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    gradient: LinearGradient(colors: [
+                      Color(0xff744ff9),
+                      Color(0xff8369de),
+                      Color(0xff8da0cb)
+                    ])),
+              )),
+          Positioned(
+              bottom: 250,
+              right: 150,
+              child: Container(
+                width: 180,
+                height: 180,
+                decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    gradient: LinearGradient(colors: [
+                      Color(0xff744ff9),
+                      Color(0xff8369de),
+                      Color(0xff8da0cb)
+                    ])),
+              )),
           Center(
-            child: ClipRect(
+            child: ClipRRect(
+              borderRadius: const BorderRadius.all(Radius.circular(25)),
               child: BackdropFilter(
                 filter: ImageFilter.blur(sigmaX: 30, sigmaY: 30),
                 child: Container(
                   width: 450,
                   height: 250,
                   decoration: BoxDecoration(
-                      color: Colors.grey.withOpacity(0.2),
+                      color: Colors.grey.withOpacity(0.1),
                       borderRadius:
                           const BorderRadius.all(Radius.circular(25))),
                 ),
